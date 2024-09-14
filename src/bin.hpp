394bee86624c2cd1b96324b2ab2429bf3ad48b95
Sandro Stiller <sandro.stiller@dragonhills.de>
2024-09-14 15:22:47 +0200

    feature: create Bin from description

--- src/bin.hpp
+++ src/bin.hpp
@@ -25,6 +25,15 @@ public:
    */
   explicit Bin(GstBin* gstBin, TransferType transferType = TransferType::None);
 
+ /**
+ * @brief Creates a Bin object from a pipeline description.
+ * @param description The GStreamer pipeline description.
+ * @param ghostUnlinkedPads whether to automatically create ghost pads for unlinked source or sink pads within the bin
+ * @return A Bin object created from the description.
+ * @throws std::runtime_error if the description is invalid or parsing fails.
+ */
+ [[nodiscard]] static Bin fromDescription(const std::string& description, bool ghostUnlinkedPads = false);
+
   /**
   * @brief create a reference to the same Bin
   * @return the new Bin with the same internal GstBin*
