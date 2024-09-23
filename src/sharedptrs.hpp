0bfebfcb703b6836f00b2b2a2230cf8df2c4912f
Sandro Stiller <sandro.stiller@gmx.de>
2024-09-23 20:19:03 +0200

    refactor: move type traits to extra header

--- src/sharedptrs.hpp
+++ src/sharedptrs.hpp
@@ -9,48 +9,15 @@
 #define DH_GST_SHAREDPTRS_HPP
 
 #include "transfertype.hpp"       // Include the external header for TransferType
+#include "typetraits.hpp"
 
-#include <gst/app/gstappsink.h>   // For GstAppSink
-#include <gst/app/gstappsrc.h>    // For GstAppSrc
 #include <gst/gst.h>              // General GStreamer types and functions
 #include <glib.h>                 // For GError, GList, GHashTable, etc.
 
 #include <memory>                 // For std::shared_ptr
 #include <stdexcept>
-#include <type_traits>            // For std::type_traits
-
 
 namespace dh::gst {
-
-// Custom trait to check if a type is a GObject-based GStreamer type
-template <typename T>
-struct IsGstObject : std::false_type {};
-
-// Specializations for GObject-based GStreamer types
-template <> struct IsGstObject<GstObject> : std::true_type {};
-template <> struct IsGstObject<GstElement> : std::true_type {};
-template <> struct IsGstObject<GstPad> : std::true_type {};
-template <> struct IsGstObject<GstBuffer> : std::true_type {};
-template <> struct IsGstObject<GstEvent> : std::true_type {};
-template <> struct IsGstObject<GstMessage> : std::true_type {};
-template <> struct IsGstObject<GstAppSink> : std::true_type {};
-template <> struct IsGstObject<GstAppSrc> : std::true_type {};
-template <> struct IsGstObject<GstBin> : std::true_type {};
-template <> struct IsGstObject<GstBus> : std::true_type {};
-template <> struct IsGstObject<GstClock> : std::true_type {};
-template <> struct IsGstObject<GstDeviceMonitor> : std::true_type {};
-template <> struct IsGstObject<GstDevice> : std::true_type {};
-template <> struct IsGstObject<GstElementFactory> : std::true_type {};
-template <> struct IsGstObject<GstIterator> : std::true_type {};
-template <> struct IsGstObject<GstPadTemplate> : std::true_type {};
-template <> struct IsGstObject<GstPipeline> : std::true_type {};
-template <> struct IsGstObject<GstPlugin> : std::true_type {};
-template <> struct IsGstObject<GstSample> : std::true_type {};
-template <> struct IsGstObject<GstPluginFeature> : std::true_type {};
-
-// Custom trait for non-GObject GStreamer types
-template <> struct IsGstObject<GstCaps> : std::false_type {};
-template <> struct IsGstObject<GstStructure> : std::false_type {};
 
 /**
  * @brief Universal Deleter for GStreamer objects.
