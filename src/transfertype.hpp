7dad9a05bc9369d0cb510505ba06fe4c872aff6b
Sandro Stiller <sandro.stiller@dragonhills.de>
2025-08-10 16:21:00 +0200

    chore: set license LGPL3+

--- src/transfertype.hpp
+++ src/transfertype.hpp
@@ -1,8 +1,21 @@
 /* -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -*-  */
-/**
- * @file transfertype.hpp
- * @author Sandro Stiller
- * @date 2024-09-14
+/* Copyright (C) 2024 Sandro Stiller <sandro.stiller@dragonhills.de>
+ * SPDX-License-Identifier: LGPL-3.0-or-later
+ *
+ * This file is part of Libdhgst <https://dragonhills.de/>.
+ *
+ * Libdhgst is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Libdhgst is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Libdhgst.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef DH_GST_TRANSFERTYPE_HPP
