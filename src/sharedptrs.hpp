57c4c4ba0b7eb51a3483117e29b2b23b0532b534
Sandro Stiller <sandro.stiller@dragonhills.de>
2026-06-04 10:10:43 +0200

    chore: switch to LGPL v2.1

--- src/sharedptrs.hpp
+++ src/sharedptrs.hpp
@@ -1,21 +1,21 @@
 /* -*- Mode: C++; indent-tabs-mode: nil; c-basic-offset: 2; tab-width: 2 -'- */
 /* Copyright (C) 2024 Sandro Stiller <sandro.stiller@dragonhills.de>
- * SPDX-License-Identifier: LGPL-3.0-or-later
+ * SPDX-License-Identifier: LGPL-2.1-or-later
  *
  * This file is part of Libdhgst <https://dragonhills.de/>.
  *
- * Libdhgst is free software: you can redistribute it and/or modify
+ * Libdhgst is free software; you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
+ * the Free Software Foundation; either version 2.1 of the License, or
  * (at your option) any later version.
  *
  * Libdhgst is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with Libdhgst.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Libdhgst. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef DH_GST_SHAREDPTRS_HPP
